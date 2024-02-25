e41eb19af17a4290f2fc920b461ef183a706a874
LocoGuy <locprv@pm.me>
2024-02-25 20:54:34 +0100

    Update priceHistory.js

--- priceHistory.js
+++ priceHistory.js
@@ -109,12 +109,8 @@ async function GetDataSimple() {
     };
   });
 
-  /* json fields
-  "id"
-  "price_change_percentage_1h_in_currency"
-  "price_change_percentage_24h_in_currency"
-  "price_change_percentage_7d_in_currency"
-    */
-  //console.log(jsonData);
-  console.log(historyInfo);
+  const historyArray = Object.entries(historyInfo);
+  historyArray.sort((a, b) => b[1].oneHour - a[1].oneHour);
+  historyInfo = Object.fromEntries(historyArray);
+  //console.log(historyInfo);
 }
